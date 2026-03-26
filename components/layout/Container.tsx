0c4c0c05d358e82df61697b32b86ae0b4839e68b
Gregor Baumgartner <gregor@bauma.si>
2026-03-26 03:47:34 +0100

    Adjust global container width

--- components/layout/Container.tsx
+++ components/layout/Container.tsx
@@ -9,7 +9,7 @@ export default function Container({ children, className = "" }: Props) {
   return (
     <div
       className={[
-        "mx-auto w-full max-w-[1120px]",
+        "mx-auto w-full max-w-[1280px]",
         "px-5 sm:px-6 lg:px-8",
         className,
       ].join(" ")}
